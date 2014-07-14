4336d8ed6b8859a64052c2223c789240e868b8a5
Michael Wei <weizhuyu@gmail.com>
2014-07-14 14:21:00 +0800

    Rounding errors in mouse movement (windows only)

--- src/mouse.c
+++ src/mouse.c
@@ -31,10 +31,10 @@ void moveMouse(MMPoint point)
 	             0, 0, 0, 0, point.x, point.y);
 	XFlush(display);
 #elif defined(IS_WINDOWS)
-	point.x *= 0xFFFF / GetSystemMetrics(SM_CXSCREEN);
-	point.y *= 0xFFFF / GetSystemMetrics(SM_CYSCREEN);
+  int x = point.x * 0xFFFF / GetSystemMetrics(SM_CXSCREEN);
+  int y = point.y * 0xFFFF / GetSystemMetrics(SM_CYSCREEN);
 	mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE,
-	            (DWORD)point.x, (DWORD)point.y, 0, 0);
+	            (DWORD)x, (DWORD)y, 0, 0);
 #endif
 }
 
